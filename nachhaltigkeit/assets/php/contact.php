d315216fedac0c0effc2c70d6429bc81805b0af8
Thierry Ollivero <thierry.ollivero@gmail.com>
2018-03-05 17:20:06 +0100

    Hop

--- nachhaltigkeit/assets/php/contact.php
+++ nachhaltigkeit/assets/php/contact.php
@@ -11,9 +11,18 @@
 	$city        	= $_POST['city'];
 	$program    	= $_POST['program'];
 
-	$message     	= "First Name: $fname, \nLast Name: $lname, \nEmail: $email, \nCell: $cell, \nAddress: $address, \nZip Code: $zip, \nCity: $city, \nProgram: $program.";
-
-	if(mail('thierry.ollivero@airliquide.com', "Test Sub", $message))
+	$message     	= "The following participant has been registered: \n \nFirst Name: $fname, \nLast Name: $lname, \nEmail: $email, \nCell: $cell, \nAddress: $address, \nZip Code: $zip, \nCity: $city, \nProgram: $program. \n \nThank you very much \nThe Air Liquide Organization Team";
+	
+	$headers  = "From: Thierry Ollivero < thierry.ollivero@airliquide.com >\n";
+    $headers .= "Cc: " .$email. "\n"; 
+    $headers .= "X-Sender: Thierry Ollivero < thierry.ollivero@airliquide.com >\n";
+    $headers .= 'X-Mailer: PHP/' . phpversion();
+    $headers .= "X-Priority: 1\n"; // Urgent message!
+    $headers .= "Return-Path: mail@testsite.com\n"; // Return path for errors
+    $headers .= "MIME-Version: 1.0\r\n";
+   
+	
+	if(mail('thierry.ollivero@airliquide.com', "Participation to Sustainability Conference", $message, $headers))
 		$form_data['success'] = true;
 	else
 		$form_data['success'] = false;
